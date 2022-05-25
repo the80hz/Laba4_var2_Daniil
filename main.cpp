a53523b4007550ecb6aaac444babe5cd2ff25c79
the80hz <dnl.vlgv@mail.ru>
2022-05-25 21:10:17 +0400

    Add menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int main(){
+    int command = 13;
+    while(true){
+        switch(command){
+            case 1: // read the file
+
+                break;
+
+            case 2: // work on the massive
+
+                break;
+
+            case 3: // save massive to the file
+
+                break;
+
+            case 4: // forming new massive (save it?)
+
+                break;
+
+            case 5: // navigation
+
+                break;
+
+            case 6: // working on current element (edit? delete?)
+
+                break;
+
+            case 7: // find element
+
+                break;
+
+            case 8: // add element (to the start? to the end?)
+
+                break;
+
+            case 10: // show menu
+
+                break;
+
+            case 0:
+                command = 0;
+
+            default:
+                break;
+        }
+
+        cout << endl << "Enter command: ";
+        cin >> command;
+        cout << endl;
+        if (command == 0) break;
+    }
     return 0;
 }
